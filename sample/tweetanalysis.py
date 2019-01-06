04d52a01535bb81a9e846c8ef2b298d4377dd803
Archel Taneka <archeltaneka@ymail.com>
2019-01-06 17:55:18 +0700

    added files for sentiment analysis

---
+++ sample/tweetanalysis.py
@@ -0,0 +1,138 @@
+import re
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import string
+import nltk
+from nltk.stem.porter import *
+from wordcloud import WordCloud
+import warnings
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+# load csv files for both train and test
+train = pd.read_csv('../src/train_E6oV3lV.csv')
+test = pd.read_csv('../src/test_tweets_anuFYb8.csv')
+
+# combine train and test set
+combi = train.append(test, ignore_index=True)
+# print(combi.head())
+
+# removing twitter handles
+def remove_pattern(input_txt, pattern):
+    r = re.findall(pattern, input_txt)
+    for i in r:
+        input_txt = re.sub(i, '', input_txt)
+    return input_txt
+
+def hashtag_extract(x):
+    hashtags = []
+    # loop the words in the tweet
+    for i in x:
+        ht = re.findall(r"#(\w+)", i)
+        hashtags.append(ht)
+    return hashtags
+
+# create new column that contains cleaned and processed tweets
+combi['tidy_tweet'] = np.vectorize(remove_pattern)(combi['tweet'], "@[\w]*")
+# remove special chars, numbers, punctuations, etc.
+combi['tidy_tweet'] = combi['tidy_tweet'].str.replace("[^a-zA-Z#]", " ")
+# remove short words -> hmm, oh, etc.
+combi['tidy_tweet'] = combi['tidy_tweet'].apply(lambda x: ' '.join([w for w in x.split() if len(w) > 3]))
+# print(combi.head())
+
+# tokenize cleaned tweets
+tokenized_tweet = combi['tidy_tweet'].apply(lambda x: x.split())
+# print(tokenized_tweet.head())
+
+# stemming --> stripping suffixes -ing, -ly, -es, -s, etc.
+stemmer = PorterStemmer()
+tokenized_tweet = tokenized_tweet.apply(lambda x: [stemmer.stem(i) for i in x])
+# print(tokenized_tweet.head())
+
+# visualization using WordCloud
+all_words = ' '.join([text for text in combi['tidy_tweet'][combi['label'] == 1]])
+wordcloud = WordCloud(width=800, height=500, random_state=21, max_font_size=110).generate(all_words)
+plt.figure(figsize=(10, 7))
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis('off')
+# plt.show()
+
+pos_words = ' '.join([text for text in combi['tidy_tweet'][combi['label'] == 0]])
+poscloud = WordCloud(width=800, height=500, random_state=21, max_font_size=110).generate(pos_words)
+plt.figure(figsize=(10, 7))
+plt.imshow(poscloud, interpolation="bilinear")
+plt.axis('off')
+# plt.show()
+
+# extracting hashtags from positive tweets
+HT_positive = hashtag_extract(combi['tidy_tweet'][combi['label'] == 0])
+# extracting hashtags from negative tweets
+HT_negative = hashtag_extract(combi['tidy_tweet'][combi['label'] == 1])
+# unnesting list
+HT_positive = sum(HT_positive, [])
+HT_negative = sum(HT_negative, [])
+
+# plot the tweets into graph
+a = nltk.FreqDist(HT_positive)
+d = pd.DataFrame({'Hashtag': list(a.keys()),
+                  'Count': list(a.values())})
+# selecting top 10 positive hashtags
+d = d.nlargest(columns="Count", n = 10)
+plt.figure(figsize=(16,5))
+ax = sns.barplot(data=d, x="Hashtag", y="Count")
+ax.set(ylabel = 'Count')
+# plt.show()
+
+# top 10 negative tweets
+b = nltk.FreqDist(HT_negative)
+e = pd.DataFrame({'Hashtag': list(b.keys()),
+                  'Count': list(b.values())})
+e = e.nlargest(columns="Count", n = 10)
+plt.figure(figsize=(16,5))
+ax = sns.barplot(data=e, x="Hashtag", y="Count")
+ax.set(ylabel = 'Count')
+# plt.show()
+
+# bag of word features
+bow_vectorizer = CountVectorizer(max_df=0.90, min_df=2, max_features=1000, stop_words='english')
+# bag-of-words feature matrix
+bow = bow_vectorizer.fit_transform(combi['tidy_tweet'])
+
+train_bow = bow[:31962,:]
+test_bow = bow[31962:,:]
+
+# splitting data into training and validation set
+xtrain_bow, xvalid_bow, ytrain, yvalid = train_test_split(train_bow, train['label'], random_state=42, test_size=0.3)
+
+lreg = LogisticRegression()
+lreg.fit(xtrain_bow, ytrain) # training the model
+
+prediction = lreg.predict_proba(xvalid_bow) # predicting on the validation set
+prediction_int = prediction[:,1] >= 0.3 # if prediction is greater than or equal to 0.3 than 1 else 0
+prediction_int = prediction_int.astype(np.int)
+print("F1 score with BOW(Bag Of Words): ", f1_score(yvalid, prediction_int)) # calculating f1 score
+
+# TF-IDF features
+tfidf_vectorizer = TfidfVectorizer(max_df=0.90, min_df=2, max_features=1000, stop_words='english')
+# TF-IDF feature matrix
+tfidf = tfidf_vectorizer.fit_transform(combi['tidy_tweet'])
+
+train_tfidf = tfidf[:31962,:]
+test_tfidf = tfidf[31962:,:]
+
+xtrain_tfidf = train_tfidf[ytrain.index]
+xvalid_tfidf = train_tfidf[yvalid.index]
+
+lreg.fit(xtrain_tfidf, ytrain)
+
+prediction = lreg.predict_proba(xvalid_tfidf)
+prediction_int = prediction[:,1] >= 0.3
+prediction_int = prediction_int.astype(np.int)
+
+print("F1 score with TF-IDF: ", f1_score(yvalid, prediction_int))
